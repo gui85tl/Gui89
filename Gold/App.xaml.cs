1a35cfbbbfdf27c404bfb491fe8241f104871077
Guilherme <gui87981@gmail.com>
2024-03-26 09:31:44 -0300

    26/03/2023

---
+++ Gold/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Gold;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Gold/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Gold;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
